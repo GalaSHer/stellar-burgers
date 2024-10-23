ded3c29c3a3217e21aaccf9eea5e387ef5347829
Galina Sheremetova <shgs001@yandex.ru>
2024-10-23 09:24:06 +0500

    доработаны тесты Cypress

--- cypress/e2e/burgerConstructor.cy.ts
+++ cypress/e2e/burgerConstructor.cy.ts
@@ -5,9 +5,15 @@ describe('тесты конструктора бургера', () => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients' });
     cy.intercept('GET', 'api/auth/user', { fixture: 'user' }).as('getUser');
     cy.intercept('POST', 'api/orders', { fixture: 'order' });
-    cy.visit('http://localhost:4000/');
+    cy.visit('/');
     cy.setCookie('accessToken', 'ACCESS_TOKEN');
     localStorage.setItem('refreshToken', 'REFRESH_TOKEN');
+
+    cy.get('[data-cy="bun"]').as('bun');
+    cy.get('[data-cy="main"]').as('main');
+    cy.get('[data-cy="sauce"]').as('sauce');
+    cy.get('[data-cy="burger-constructor"]').as('constructor');
+    cy.get('[data-cy="burger-constructor-ingredients"]').as('ingredients');
   });
 
   afterEach(() => {
@@ -16,7 +22,7 @@ describe('тесты конструктора бургера', () => {
   });
 
   it('отображается список ингредиентов', () => {
-    cy.get('[data-cy="bun"]').should('have.length.at.least', 1);
+    cy.get('@bun').should('have.length.at.least', 1);
     cy.get('[data-cy="main"],[data-cy="sauce"]').should(
       'have.length.at.least',
       1
@@ -24,32 +30,32 @@ describe('тесты конструктора бургера', () => {
   });
 
   it('добавляет булку в конструктор', () => {
-    cy.get('[data-cy=burger-constructor]').should('not.contain', 'булка');
+    cy.get('@constructor').should('not.contain', 'булка');
     cy.get('[data-cy=bun] button').first().click();
-    cy.get(`[data-cy=burger-constructor]`).contains('булка');
+    cy.get('@constructor').contains('булка');
   });
 
   it('добавляет ингредиент в конструктор', () => {
-    cy.get('[data-cy=burger-constructor-ingredients]').should('have.length', 0);
+    cy.get('@ingredients').should('not.contain', 'Биокотлета');
     cy.get('[data-cy=main] button').first().click();
-    cy.get(`[data-cy=burger-constructor-ingredients]`).contains('Биокотлета');
+    cy.get('@ingredients').contains('Биокотлета');
   });
 
   it('добавляет соус в конструктор', () => {
-    cy.get('[data-cy=burger-constructor-ingredients]').should('have.length', 0);
+    cy.get('@ingredients').should('not.contain', 'Соус');
     cy.get('[data-cy=sauce] button').first().click();
-    cy.get(`[data-cy=burger-constructor-ingredients]`).contains('Соус');
+    cy.get(`@ingredients`).contains('Соус');
   });
 
   it('открывает описание ингредиента в модальном окне', () => {
-    cy.get('[data-cy=bun').first().click();
+    cy.get('@bun').first().click();
     cy.get('[data-cy=modal]')
       .should('be.visible')
       .should('contain', 'Краторная булка');
   });
 
   it('закрывает описание ингредиента в модальном окне по крестику', () => {
-    cy.get('[data-cy=bun').first().click();
+    cy.get('@bun').first().click();
     cy.get('[data-cy=modal]').should('be.visible');
     cy.get('[data-cy=modal-close]').click();
     cy.get('[data-cy=modal]').should('not.exist');
@@ -67,10 +73,7 @@ describe('тесты конструктора бургера', () => {
     cy.wait(500);
     cy.get('[data-cy=modal-close]').click();
     cy.get('[data-cy=modal]').should('not.exist');
-    cy.get('[data-cy=burger-constructor-ingredients]').should(
-      'contain',
-      'Выберите начинку'
-    );
-    cy.get('[data-cy=burger-constructor]').should('contain', 'Выберите булки');
+    cy.get('@ingredients').should('contain', 'Выберите начинку');
+    cy.get('@constructor').should('contain', 'Выберите булки');
   });
 });
